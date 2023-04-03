1f270107c6c861f426e62ae9be437993ac840739
Apisapple <38449976+Apisapple@users.noreply.github.com>
2023-04-03 21:24:48 +0900

    [CHORE] Init project
    Init project

---
+++ my-series/src/main/java/com/example/myseries/MySeriesApplication.java
@@ -0,0 +1,13 @@
+package com.example.myseries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySeriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySeriesApplication.class, args);
+	}
+
+}
---
+++ my-series/src/test/java/com/example/myseries/MySeriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myseries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySeriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
