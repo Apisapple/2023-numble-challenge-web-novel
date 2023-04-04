f7c1828fe5e6c381c01366aea001d9de6bca1652
Apisapple <38449976+Apisapple@users.noreply.github.com>
2023-04-04 23:54:20 +0900

    [FEAT] Create Novel
    Create Novel entity

---
+++ my-series/src/main/java/com/example/myseries/novel/entity/Novel.java
@@ -0,0 +1,33 @@
+package com.example.myseries.novel.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "NOVEL")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Novel {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "NOVEL_ID", nullable = false)
+  private Long id;
+  @Column(name = "NOVEL_TITEL", nullable = false)
+  private String novelTitle;
+  @Column(name = "AUTHOR", nullable = false)
+  private String author;
+  @Column(name = "NOVEL_GRADE", nullable = false)
+  private Float novelGrade;
+
+  @Builder
+  public Novel(String novelTitle, String author) {
+    this.novelTitle = novelTitle;
+    this.author = author;
+    this.novelGrade = 0.0f;
+  }
+}
