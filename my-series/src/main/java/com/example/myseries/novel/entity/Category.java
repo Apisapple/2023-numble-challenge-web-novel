ea4dd7bb0c032226683cf67ae94b5870944a0d16
Apisapple <kimminyong2034@gmail.com>
2023-04-05 01:01:01 +0900

    [FEAT] Add NovelCategory
    Add NovelCategory Entity

--- my-series/src/main/java/com/example/myseries/novel/entity/Category.java
+++ my-series/src/main/java/com/example/myseries/novel/entity/Category.java
@@ -1,4 +1,35 @@
 package com.example.myseries.novel.entity;
 
-public enum Category {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "NOVEL_CATEGORY")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Category {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "CATEGORY_ID")
+  private Long id;
+
+  @Column(name = "CATEGORY", nullable = false, length = 255)
+  private String category;
+
+  @OneToMany(mappedBy = "category")
+  private List<NovelCategory> novelCategories;
+  @Builder
+  public Category(String category) {
+    this.category = category;
+  }
+
 }
---
+++ my-series/src/main/java/com/example/myseries/novel/entity/NovelCategory.java
@@ -0,0 +1,34 @@
+package com.example.myseries.novel.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class NovelCategory {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  @JoinColumn(name = "NOVEL_ID")
+  @ManyToOne(fetch = FetchType.LAZY)
+  private Novel novel;
+
+  @JoinColumn(name = "CATEGORY_ID")
+  @ManyToOne(fetch = FetchType.LAZY)
+  private Category category;
+
+  public NovelCategory(Novel novel, Category category) {
+    this.novel = novel;
+    this.category = category;
+  }
+}
