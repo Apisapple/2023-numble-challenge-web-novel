42d8e8eb83fb80f0f8e81cbff82d37cfe925e5d6
Apisapple <38449976+Apisapple@users.noreply.github.com>
2023-04-06 00:21:13 +0900

    [FEAT] Create Repository
    Create Novel, NovelCategory, Category repository

---
+++ my-series/src/main/java/com/example/myseries/novel/repository/NovelCategoryRepository.java
@@ -0,0 +1,8 @@
+package com.example.myseries.novel.repository;
+
+import com.example.myseries.novel.model.entity.NovelCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NovelCategoryRepository extends JpaRepository<NovelCategory, Long> {
+
+}
