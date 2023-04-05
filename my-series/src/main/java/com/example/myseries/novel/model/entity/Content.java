ba505198b286dcbf385f68caf3f5a4aa91002e49
Apisapple <38449976+Apisapple@users.noreply.github.com>
2023-04-06 00:18:46 +0900

    [FEAT] Add Content
    - Content 추가
    - Novel entity에 AuditingEntityListener 추가

--- my-series/src/main/java/com/example/myseries/novel/model/entity/Content.java
+++ my-series/src/main/java/com/example/myseries/novel/model/entity/Content.java
@@ -1,5 +1,41 @@
 package com.example.myseries.novel.model.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class Content {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  private Novel novel;
+  
+  private String title;
+
+  private String contentValue;
+
+  private Float contentGrade;
+
+  @Builder
+  public Content(String title, String contentValue) {
+    this.title = title;
+    this.contentValue = contentValue;
+    this.contentGrade = 0.0f;
+  }
 }
