bb515ad9231d68906ee88d459bc3dd8490ef0e18
Apisapple <38449976+Apisapple@users.noreply.github.com>
2023-04-08 02:34:44 +0900

    [FIX] Delete getAll
    불필요한 함수 삭제

--- my-series/src/main/java/com/example/myseries/novel/repository/CategoryRepository.java
+++ my-series/src/main/java/com/example/myseries/novel/repository/CategoryRepository.java
@@ -1,11 +1,9 @@
 package com.example.myseries.novel.repository;
 
 import com.example.myseries.novel.model.entity.Category;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
   Optional<Category> findCategoryByValue(String value);
-  List<Category> getAll
 }
