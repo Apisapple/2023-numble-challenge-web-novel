be02e661c2225f981f0b21564316b891c1c991ed
Apisapple <38449976+Apisapple@users.noreply.github.com>
2023-04-08 02:31:12 +0900

    [FEAT] Add findNovelByNovelTitle
    findNovelByNovelTitle 추가

--- my-series/src/main/java/com/example/myseries/novel/repository/NovelRepository.java
+++ my-series/src/main/java/com/example/myseries/novel/repository/NovelRepository.java
@@ -1,8 +1,10 @@
 package com.example.myseries.novel.repository;
 
 import com.example.myseries.novel.model.entity.Novel;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NovelRepository extends JpaRepository<Novel, Long> {
 
+  Optional<Novel> findNovelByNovelTitle(String title);
 }
