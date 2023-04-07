02be480cfc37b006138b4f1d0585421a96e4f435
Apisapple <38449976+Apisapple@users.noreply.github.com>
2023-04-08 02:19:45 +0900

    [FEAT] Update CategoryConverter
    Add collection converter

--- my-series/src/main/java/com/example/myseries/converter/CategoryConverter.java
+++ my-series/src/main/java/com/example/myseries/converter/CategoryConverter.java
@@ -2,6 +2,9 @@ package com.example.myseries.converter;
 
 import com.example.myseries.novel.model.dto.CategoryDto;
 import com.example.myseries.novel.model.entity.Category;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class CategoryConverter extends Converter<CategoryDto, Category> {
 
@@ -11,7 +14,6 @@ public class CategoryConverter extends Converter<CategoryDto, Category> {
 
   private static CategoryDto convertToDto(Category category) {
     return CategoryDto.builder()
-        .id(category.getId())
         .value(category.getValue())
         .build();
   }
@@ -20,5 +22,13 @@ public class CategoryConverter extends Converter<CategoryDto, Category> {
     return Category.builder()
         .value(categoryDto.getValue())
         .build();
+  }
+
+  public final List<Category> createFromDtos(final Collection<CategoryDto> dtos) {
+    return dtos.stream().map(this::convertFromDto).collect(Collectors.toList());
+  }
+
+  public final List<CategoryDto> createFromEntities(final Collection<Category> entities) {
+    return entities.stream().map(this::convertFromEntity).collect(Collectors.toList());
   }
 }
