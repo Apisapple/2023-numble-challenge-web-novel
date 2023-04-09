90faea4419da925d43128de377529bb111b9acf2
Apisapple <38449976+Apisapple@users.noreply.github.com>
2023-04-09 23:28:32 +0900

    [FEAT] Add EpisodeConverter
    EpisodeConverter 추가

--- my-series/src/main/java/com/example/myseries/novel/model/dto/EpisodeDto.java
+++ my-series/src/main/java/com/example/myseries/novel/model/dto/EpisodeDto.java
@@ -1,5 +1,31 @@
 package com.example.myseries.novel.model.dto;
 
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Setter
+@Getter
+@Builder
+@ToString
+@AllArgsConstructor
 public class EpisodeDto {
 
+  private Long id;
+
+  private Long novelId;
+
+  private String title;
+
+  private String content;
+
+  private Float grade;
+
+  private LocalDateTime createdDate;
+
+  private LocalDateTime modifiedDate;
 }
