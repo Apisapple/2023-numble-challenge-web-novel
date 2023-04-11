f005d6d43e61a2d8da70c39fb0e6ebc5f51a5236
Apisapple <38449976+Apisapple@users.noreply.github.com>
2023-04-11 23:48:58 +0900

    [FEAT] Update MemberController
    Swagger 설명 추가

--- my-series/src/main/java/com/example/myseries/member/controller/MemberController.java
+++ my-series/src/main/java/com/example/myseries/member/controller/MemberController.java
@@ -2,36 +2,64 @@ package com.example.myseries.member.controller;
 
 import com.example.myseries.member.dto.MemberDto;
 import com.example.myseries.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/member")
 public class MemberController {
 
   private final MemberService memberService;
 
-  @GetMapping("/api/member/{id}")
+  @Operation(summary = "Get Member information by id",
+      description = "Get Member information by id")
+  @ApiResponse(responseCode = "200", description = "OK.")
+  @ApiResponse(responseCode = "400", description = "BAD REQUEST.")
+  @ApiResponse(responseCode = "404", description = "NOT FOUND CALENDAR INFORMATION BY ID.")
+  @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR.")
+  @GetMapping("/{id}")
   public ResponseEntity<MemberDto> getMemberById(@PathVariable Long id) {
     return ResponseEntity.ok(memberService.getMemberById(id));
   }
 
-  @PostMapping("/api/member/login")
+  @Operation(summary = "Login",
+      description = "Try login by user name")
+  @ApiResponse(responseCode = "200", description = "OK.")
+  @ApiResponse(responseCode = "400", description = "BAD REQUEST.")
+  @ApiResponse(responseCode = "404", description = "NOT FOUND CALENDAR INFORMATION BY ID.")
+  @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR.")
+  @PostMapping("/login")
   public ResponseEntity<MemberDto> login(@RequestBody String name) {
     return ResponseEntity.ok(memberService.getMemberByName(name));
   }
 
-  @PostMapping("/api/point/charge")
+  @Operation(summary = "Charging point",
+      description = "Charging point")
+  @ApiResponse(responseCode = "200", description = "OK.")
+  @ApiResponse(responseCode = "400", description = "BAD REQUEST.")
+  @ApiResponse(responseCode = "404", description = "NOT FOUND CALENDAR INFORMATION BY ID.")
+  @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR.")
+  @PostMapping("/point/charge")
   public ResponseEntity<MemberDto> chargePoint(@RequestBody MemberDto memberDto) {
     return ResponseEntity.ok(memberService.buyPoint(memberDto.getName(), memberDto.getPoint()));
   }
 
-  @PostMapping("/api/point/use")
+  @Operation(summary = "Using point",
+      description = "Using point")
+  @ApiResponse(responseCode = "200", description = "OK.")
+  @ApiResponse(responseCode = "400", description = "BAD REQUEST.")
+  @ApiResponse(responseCode = "404", description = "NOT FOUND CALENDAR INFORMATION BY ID.")
+  @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR.")
+  @PostMapping("/point/use")
   public ResponseEntity<MemberDto> usePoint(@RequestBody MemberDto memberDto) {
     return ResponseEntity.ok(memberService.buyPoint(memberDto.getName(), memberDto.getPoint()));
   }
