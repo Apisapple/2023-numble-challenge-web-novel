8259a3d59eb68d71fb205e524fdcada3365c19b8
Apisapple <kimminyong2034@gmail.com>
2023-04-15 15:25:03 +0900

    [FEAT] : NovelDto toEntity 추가
    NovelDto에 toEntity 함수 추가

--- my-series/src/main/java/com/example/myseries/novel/model/dto/NovelDto.java
+++ my-series/src/main/java/com/example/myseries/novel/model/dto/NovelDto.java
@@ -1,6 +1,8 @@
 package com.example.myseries.novel.model.dto;
 
 import com.example.myseries.member.dto.MemberDto;
+import com.example.myseries.member.entity.Member;
+import com.example.myseries.novel.model.entity.Novel;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import lombok.Builder;
@@ -15,4 +17,12 @@ public class NovelDto implements Serializable {
   private final Float grade;
   private final LocalDateTime createdDate;
   private final LocalDateTime modifiedDate;
+
+  public Novel toEntity(Member author) {
+    return Novel.builder()
+        .title(this.title)
+        .author(author)
+        .build();
+  }
+
 }
