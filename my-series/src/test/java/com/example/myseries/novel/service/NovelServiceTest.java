a8d642bf0a7de9c1f7518e81986fb110091c3dc8
Apisapple <kimminyong2034@gmail.com>
2023-04-15 16:23:42 +0900

    [TEST] : 소설 정보 변경 테스트 함수 추가
    소설 정보 변경 테스트 함수 추가

--- my-series/src/test/java/com/example/myseries/novel/service/NovelServiceTest.java
+++ my-series/src/test/java/com/example/myseries/novel/service/NovelServiceTest.java
@@ -11,12 +11,14 @@ import com.example.myseries.member.dto.MemberDto;
 import com.example.myseries.member.entity.Member;
 import com.example.myseries.member.repository.MemberRepository;
 import com.example.myseries.novel.model.dto.NovelDto;
+import com.example.myseries.novel.model.dto.UpdateNovelRequestData;
 import com.example.myseries.novel.model.entity.Novel;
 import com.example.myseries.novel.repository.NovelRepository;
 import java.util.Optional;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -49,7 +51,7 @@ class NovelServiceTest {
 
     when(novel.getId()).thenReturn(1L);
     when(novel.getAuthor()).thenReturn(author);
-    when(novel.getTitle()).thenReturn("test_title");
+//    when(novel.getTitle()).thenReturn("test_title");
     when(novel.getGrade()).thenReturn(0.0f);
     when(novel.toDto()).thenReturn(novelDto);
 
@@ -60,6 +62,7 @@ class NovelServiceTest {
   }
 
   @Test
+  @DisplayName(value = "소설 추가 함수")
   void writeNovel() {
     given(memberRepository.findByName(any())).willReturn(Optional.of(author));
     given(novelRepository.existsNovelByTitle(novelDto.getTitle())).willReturn(false);
@@ -70,5 +73,28 @@ class NovelServiceTest {
     NovelDto testResult = novelService.writeNovel(novelDto);
 
     Assertions.assertNotNull(testResult);
+  }
+
+  @Test
+  @DisplayName(value = "소설 업데이트 테스트")
+  void updateNovelInformation() {
+    UpdateNovelRequestData request = mock(UpdateNovelRequestData.class);
+    Novel savedNovel = Novel.builder()
+        .author(author)
+        .title("test_title")
+        .build();
+
+    when(request.getAuthor()).thenReturn("test_user");
+    when(request.getAsIsTitle()).thenReturn("test_title");
+    when(request.getToBeTitle()).thenReturn("update_title");
+
+    given(memberRepository.findByName(any())).willReturn(Optional.of(author));
+    given(novelRepository.findNovelByTitleAndAuthor(request.getAsIsTitle(), author))
+        .willReturn(Optional.ofNullable(savedNovel));
+
+    NovelDto result = novelService.updateNovelInformation(request);
+
+    Assertions.assertNotNull(result);
+    Assertions.assertEquals(request.getToBeTitle(), result.getTitle());
   }
 }
