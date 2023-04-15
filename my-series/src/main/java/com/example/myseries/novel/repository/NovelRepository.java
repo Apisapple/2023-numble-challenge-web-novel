95d3af75d6a48d67ca672242d3fd4ed86aea8666
Apisapple <kimminyong2034@gmail.com>
2023-04-15 15:25:42 +0900

    [FEAT] : findNovelByTitleAndAuthor 추가
    소설의 제목과 작가 정보로 정보 조회 함수 추가

--- my-series/src/main/java/com/example/myseries/novel/repository/NovelRepository.java
+++ my-series/src/main/java/com/example/myseries/novel/repository/NovelRepository.java
@@ -1,5 +1,6 @@
 package com.example.myseries.novel.repository;
 
+import com.example.myseries.member.entity.Member;
 import com.example.myseries.novel.model.entity.Novel;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,6 +8,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface NovelRepository extends JpaRepository<Novel, Long> {
 
   Optional<Novel> findNovelByTitle(String title);
+
+  Optional<Novel> findNovelByTitleAndAuthor(String title, Member author);
 
   Optional<Novel> findNovelById(Long id);
 
