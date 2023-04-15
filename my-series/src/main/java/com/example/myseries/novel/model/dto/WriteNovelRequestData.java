d6d2fc4a333fe1d109353d7d9091212c25d4f268
Apisapple <38449976+Apisapple@users.noreply.github.com>
2023-04-16 04:40:08 +0900

    [FEAT] WriteNovelRequestData 추가
    WriteNovelRequestData 추가

---
+++ my-series/src/main/java/com/example/myseries/novel/model/dto/WriteNovelRequestData.java
@@ -0,0 +1,10 @@
+package com.example.myseries.novel.model.dto;
+
+import lombok.Data;
+
+@Data
+public class WriteNovelRequestData {
+
+  private final String author;
+  private final String title;
+}
