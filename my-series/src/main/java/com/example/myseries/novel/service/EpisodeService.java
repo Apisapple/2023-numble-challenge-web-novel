d0aaccdb92550b826fc318747f48a8127c96f044
Apisapple <38449976+Apisapple@users.noreply.github.com>
2023-05-03 00:43:52 +0900

    [FEAT] : Update writeEpisode
     Update writeEpisode

--- my-series/src/main/java/com/example/myseries/novel/service/EpisodeService.java
+++ my-series/src/main/java/com/example/myseries/novel/service/EpisodeService.java
@@ -1,6 +1,9 @@
 package com.example.myseries.novel.service;
 
 
+import com.example.myseries.novel.model.entity.Episode;
+import com.example.myseries.novel.model.entity.EpisodePage;
+import com.example.myseries.novel.model.record.EpisodeRequestData;
 import com.example.myseries.novel.repository.EpisodeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,10 +13,29 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class EpisodeService {
+
   private final EpisodeRepository episodeRepository;
+  private final int contentLength = 500;
 
-  public void writeEpisode() {
+  public void writeEpisode(EpisodeRequestData requestData) {
+    Episode episode = Episode.builder()
+        .title(requestData.episodeTitle())
+        .build();
 
+    String[] splitContents = requestData.content().split("(?<=\\G.{" + contentLength + "})");
+
+    int indexPage = 0;
+    for (String splitContent : splitContents) {
+      EpisodePage episodePage = EpisodePage.builder()
+          .content(splitContent)
+          .episode(episode)
+          .pageNumber(indexPage + 1)
+          .build();
+
+      episode.addEpisodePage(episodePage);
+      indexPage++;
+    }
+    episodeRepository.save(episode);
   }
 
   public void modifyEpisode() {
