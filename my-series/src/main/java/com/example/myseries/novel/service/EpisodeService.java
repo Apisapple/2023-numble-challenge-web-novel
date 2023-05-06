0b8882612b5aa92df2d088244ebd74d24a5a27eb
Apisapple <38449976+Apisapple@users.noreply.github.com>
2023-05-07 00:44:24 +0900

    [FEAT] : EpisodeService 수정
    writeEpisode 함수 수정

--- my-series/src/main/java/com/example/myseries/novel/service/EpisodeService.java
+++ my-series/src/main/java/com/example/myseries/novel/service/EpisodeService.java
@@ -1,29 +1,37 @@
 package com.example.myseries.novel.service;
 
 
+import com.example.myseries.novel.model.dto.EpisodeDto;
 import com.example.myseries.novel.model.entity.Episode;
 import com.example.myseries.novel.model.entity.EpisodePage;
+import com.example.myseries.novel.model.entity.Novel;
 import com.example.myseries.novel.model.record.EpisodeRequestData;
 import com.example.myseries.novel.repository.EpisodeRepository;
+import com.example.myseries.novel.repository.NovelRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
+@Slf4j
 public class EpisodeService {
 
   private final EpisodeRepository episodeRepository;
+  private final NovelRepository novelRepository;
   private final int contentLength = 500;
 
-  public void writeEpisode(EpisodeRequestData requestData) {
+  public EpisodeDto writeEpisode(EpisodeRequestData requestData) throws Exception {
+    Novel novel = novelRepository.findNovelById(requestData.novelId())
+        .orElseThrow(() -> new IllegalArgumentException(""));
+
     Episode episode = Episode.builder()
         .title(requestData.episodeTitle())
         .build();
 
     String[] splitContents = requestData.content().split("(?<=\\G.{" + contentLength + "})");
-
     int indexPage = 0;
     for (String splitContent : splitContents) {
       EpisodePage episodePage = EpisodePage.builder()
@@ -35,7 +43,8 @@ public class EpisodeService {
       episode.addEpisodePage(episodePage);
       indexPage++;
     }
-    episodeRepository.save(episode);
+    novel.addEpisode(episode);
+    return episodeRepository.save(episode).toDto();
   }
 
   public void modifyEpisode() {
