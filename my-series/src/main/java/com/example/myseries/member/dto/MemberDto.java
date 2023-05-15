a0d24f97ee15ea87e7ad4c98780fd0355c0774c2
Apisapple <38449976+Apisapple@users.noreply.github.com>
2023-05-15 23:53:02 +0900

    [FEAT] : Update toEntity function
    toEntity 함수에 Sha512 기능 추가

--- my-series/src/main/java/com/example/myseries/member/dto/MemberDto.java
+++ my-series/src/main/java/com/example/myseries/member/dto/MemberDto.java
@@ -3,6 +3,8 @@ package com.example.myseries.member.dto;
 import com.example.myseries.member.entity.Member;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import java.math.BigInteger;
+import java.security.MessageDigest;
 import lombok.Builder;
 import lombok.Data;
 import lombok.ToString;
@@ -24,10 +26,27 @@ public class MemberDto {
   private Integer point;
 
   public Member toEntity() {
+    String encryptPassword = getSHA512(this.email + this.password);
+
     return Member.builder()
         .email(this.email)
-        .password(this.password)
+        .password(encryptPassword)
         .name(this.name)
         .build();
+  }
+
+  private String getSHA512(String input) {
+
+    String result = "";
+    try {
+      MessageDigest digest = MessageDigest.getInstance("SHA-512");
+      digest.reset();
+      digest.update(input.getBytes("utf8"));
+      result = String.format("%0128x", new BigInteger(1, digest.digest()));
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    return result;
   }
 }
