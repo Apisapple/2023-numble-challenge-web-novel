ab8ca7e5da9072df86984763edcec70bbdb6b6f3
Apisapple <38449976+Apisapple@users.noreply.github.com>
2023-06-07 01:28:41 +0900

    Update MemberServiceTest
    Member service 테스트 코드 추가

--- my-series/src/test/java/com/example/myseries/member/service/MemberServiceTest.java
+++ my-series/src/test/java/com/example/myseries/member/service/MemberServiceTest.java
@@ -2,11 +2,11 @@ package com.example.myseries.member.service;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
+import com.example.myseries.member.dto.MemberDto;
 import com.example.myseries.member.entity.Member;
 import com.example.myseries.member.repository.MemberRepository;
-import java.util.Optional;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -22,11 +22,21 @@ class MemberServiceTest {
   @Mock
   private MemberRepository memberRepository;
 
-  @Test
-  void saveMember() {
-  }
 
   @Test
-  void usePoint() {
+  void signup() {
+    // TODO : 테스트 코드 추가 작성 필요
+    
+    MemberDto memberDto = MemberDto.builder()
+        .name("test_user")
+        .password("1234qwer")
+        .point(0)
+        .id(1L)
+        .build();
+
+    Member member = mock(Member.class);
+    given(memberRepository.save(any())).willReturn(member);
+
+    memberService.signup(memberDto);
   }
 }
