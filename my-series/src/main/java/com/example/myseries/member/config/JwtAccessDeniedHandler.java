9d0baad420e0ff98c90f93952efcca69364b8808
Apisapple <38449976+Apisapple@users.noreply.github.com>
2023-06-13 00:42:43 +0900

    Add Component annotation
    Component anotation 추가

--- my-series/src/main/java/com/example/myseries/member/config/JwtAccessDeniedHandler.java
+++ my-series/src/main/java/com/example/myseries/member/config/JwtAccessDeniedHandler.java
@@ -6,7 +6,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
 
+@Component
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
 
   @Override
